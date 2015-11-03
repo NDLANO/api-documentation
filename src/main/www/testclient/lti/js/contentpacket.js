9d8f8e2018409eeb3908a2ffaac387f4ac93d8aa
Thomas Tøkje <thomas.tokje@knowit.no>
2015-11-03 12:30:26 +0100

    NDLA-51: slideshow har nå blitt gjort helt generisk -- alt som trengs, er å kalle funksjonen prepareSlideshow() med slideshowets rotnode som argument. I tillegg må man legge til events til de UI-elementene som skal brukes til å styre slideshowet.

--- src/main/www/testclient/lti/js/contentpacket.js
+++ src/main/www/testclient/lti/js/contentpacket.js
@@ -2,27 +2,27 @@ function range(num){
     return Array.apply(null, Array(num)).map(function(_, i){return i;});
 }
 
-function updateSlideshowProgressBar(){
-    var numSlides = $("#slideshow .slide").size();
-    var progressBarWidth = $(".slideshow .slideshow-progressbar").width();
-    var progressBarHeight = $(".slideshow .slideshow-progressbar").height();
-    $("#slideshow-container .slideshow .slideshow-progressbar svg")
+function updateSlideshowProgressBar(slideshowContainer){
+    var numSlides = slideshowContainer.find(".slideshow .slide").size();
+    var progressBarWidth = slideshowContainer.find(".slideshow .slideshow-progressbar").width();
+    var progressBarHeight = slideshowContainer.find(".slideshow .slideshow-progressbar").height();
+    slideshowContainer.find(".slideshow .slideshow-progressbar svg")
         .attr("width", progressBarWidth)
         .attr("height", progressBarHeight);
     var backgroundLinePadding = progressBarHeight / 2;
     var slideSpacing = (progressBarWidth - 2*backgroundLinePadding) / (numSlides-1);
     var slideXPositions = range(numSlides).map(function(i){return backgroundLinePadding + (i * slideSpacing);});
-    $("#slideshow-container .slideshow .progressbar-slide")
+    slideshowContainer.find(".slideshow .progressbar-slide")
         .attr("cx", function(index) {
             return slideXPositions[$(this).attr("index")];
         });
-    $("#slideshow-container .slideshow .progressbar-backgroundline")
+    slideshowContainer.find(".slideshow .progressbar-backgroundline")
         .attr("x2", progressBarWidth - backgroundLinePadding);
 }
-function drawFrontpageProgressBar(){
-    var progressBar = $("#slideshow-container .frontpage-progressbar");
-    var progressBarWidth = $("#slideshow-container .frontpage-progressbar-container").width();
-    var progressBarHeight = $("#slideshow-container .frontpage-progressbar-container").height();
+function drawFrontpageProgressBar(slideshowContainer){
+    var progressBar = slideshowContainer.find(".frontpage-progressbar");
+    var progressBarWidth = slideshowContainer.find(".frontpage-progressbar-container").width();
+    var progressBarHeight = slideshowContainer.find(".frontpage-progressbar-container").height();
     progressBar
         .attr("width", progressBarWidth)
         .attr("height", progressBarHeight);
@@ -37,8 +37,7 @@ function drawFrontpageProgressBar(){
         .attr("stroke", "rgb(96,96,96)")
         .attr("stroke-width", "2");
     progressBar.find(".progressbar-backgroundline-group").append(backgroundLine);
-    var numSlides = $("#slideshow-container .slideshow-frontpage-content .frontpage-slide").size();
-    console.log("number of slides: " + numSlides);
+    var numSlides = slideshowContainer.find(".slideshow-frontpage-content .frontpage-slide").size();
     var progressBarSlidesGroup = progressBar.find(".progressbar-slides-group");
     var slideSpacing = (progressBarHeight - 2 * padding) / (numSlides-1);
     var slideYPositions = range(numSlides).map(function(i){return padding + (i * slideSpacing);});
@@ -58,12 +57,12 @@ function drawFrontpageProgressBar(){
     }
 }
 
-function drawSlideshowProgressBar(){
-    var progressBar = $("#slideshow-container .slideshow .slideshow-progressbar svg");
+function drawSlideshowProgressBar(slideshowContainer){
+    var progressBar = slideshowContainer.find(".slideshow .slideshow-progressbar svg");
     progressBar.find(".progressbar-backgroundline-group").empty();
     progressBar.find(".progressbar-slides-group").empty();
-    var progressBarWidth = $("#slideshow-container .slideshow .slideshow-progressbar").width();
-    var progressBarHeight = $("#slideshow-container .slideshow .slideshow-progressbar").height();
+    var progressBarWidth = slideshowContainer.find(".slideshow .slideshow-progressbar").width();
+    var progressBarHeight = slideshowContainer.find(".slideshow .slideshow-progressbar").height();
     progressBar
         .attr("width", progressBarWidth)
         .attr("height", progressBarHeight);
@@ -79,7 +78,7 @@ function drawSlideshowProgressBar(){
         .attr("stroke-width", "2");
     progressBar.find(".progressbar-backgroundline-group").append(backgroundLine);
     var progressBarSlidesGroup = progressBar.find(".progressbar-slides-group");
-    var numSlides = $("#slideshow-container .slideshow .slide").size();
+    var numSlides = slideshowContainer.find(".slideshow .slide").size();
     var slideSpacing = (progressBarWidth - 2*backgroundLinePadding) / (numSlides-1);
     var slideXPositions = range(numSlides).map(function(i){return backgroundLinePadding + (i * slideSpacing);});
     for(var key in slideXPositions){
@@ -97,107 +96,114 @@ function drawSlideshowProgressBar(){
         progressBarSlidesGroup.append(circle);
     }
 
-    $("#slideshow-container .slideshow .slideshow-progressbar svg .progressbar-slide").attr("fill", "url(#inactiveSlideGradient)");
+    slideshowContainer.find(".slideshow .slideshow-progressbar svg .progressbar-slide").attr("fill", "url(#inactiveSlideGradient)");
 }
 
-function updateSlideshowState(){
-    var numSlides = $("#slideshow-container .slideshow .slide").size();
+function updateSlideshowState(slideshowContainer){
+    var numSlides = slideshowContainer.find(".slideshow .slide").size();
 
     // If on last slide, hide 'next' button
-    if($("#slideshow-container .slideshow .progressbar-slide:first").attr("index") == numSlides-1) {
-        $("#slideshow-container .slideshow .previous-next-container .next").hide();
+    if(slideshowContainer.find(".slideshow .progressbar-slide:first").attr("index") == numSlides-1) {
+        slideshowContainer.find(".slideshow .previous-next-container .next").hide();
     } else {
-        $("#slideshow-container .slideshow .previous-next-container .next").show();
+        slideshowContainer.find(".slideshow .previous-next-container .next").show();
     }
 
     // If on first slide, hide 'previous' button
-    if($("#slideshow-container .slideshow .progressbar-slide:first").attr("index") == 0) {
-        $("#slideshow-container .slideshow .previous-next-container .prev").hide();
+    if(slideshowContainer.find(".slideshow .progressbar-slide:first").attr("index") == 0) {
+        slideshowContainer.find(".slideshow .previous-next-container .prev").hide();
     } else {
-        $("#slideshow-container .slideshow .previous-next-container .prev").show();
+        slideshowContainer.find(".slideshow .previous-next-container .prev").show();
     }
 }
 
-function indexSlides(){
-    $("#slideshow-container .slideshow .slide")
+function indexSlides(slideshowContainer){
+    $(slideshowContainer.find(".slideshow .slide")
         .each(function(slide){
             $(this).attr("index", slide);
-        });
+        }));
 }
 
-function nextSlide(){
-    $("#slideshow-container .slideshow .slide:first")
+function nextSlide(slideshowContainer){
+    slideshowContainer.find(".slideshow .slide:first")
         .hide()
         .next()
         .show()
         .end()
-        .appendTo("#slideshow-container .slideshow > .slideshow-slides-group");
-    $("#slideshow-container .slideshow .slideshow-progressbar > svg .progressbar-slide:first")
+        .appendTo(slideshowContainer.find(".slideshow > .slideshow-slides-group"));
+    slideshowContainer.find(".slideshow .slideshow-progressbar > svg .progressbar-slide:first")
         .attr("fill", "url(#inactiveSlideGradient)")
         .next()
         .attr("fill", "url(#activeSlideGradient)")
         .end()
-        .appendTo("#slideshow-container .slideshow-progressbar > svg > .progressbar-slides-group");
-    updateSlideshowState();
+        .appendTo(slideshowContainer.find(".slideshow-progressbar > svg > .progressbar-slides-group"));
+    updateSlideshowState(slideshowContainer);
 }
-function prevSlide(){
-    $("#slideshow-container .slideshow .slide")
+function prevSlide(slideshowContainer){
+    slideshowContainer.find(".slideshow .slide")
         .first()
         .hide()
         .end()
         .last()
         .show()
-        .prependTo("#slideshow-container .slideshow > .slideshow-slides-group");
-    $("#slideshow-container .slideshow .slideshow-progressbar svg .progressbar-slide")
+        .prependTo(slideshowContainer.find(".slideshow > .slideshow-slides-group"));
+    slideshowContainer.find(".slideshow .slideshow-progressbar svg .progressbar-slide")
         .attr("fill", "url(#inactiveSlideGradient)")
         .last()
         .attr("fill", "url(#activeSlideGradient)")
-        .prependTo("#slideshow-container .slideshow-progressbar > svg > .progressbar-slides-group");
-    updateSlideshowState();
+        .prependTo(slideshowContainer.find(".slideshow-progressbar > svg > .progressbar-slides-group"));
+    updateSlideshowState(slideshowContainer);
 }
 
-function setStateToFrontpage(){
-    $("#slideshow-container .slideshow").hide();
-    $("#slideshow-container .slideshow-frontpage").show();
+function setStateToFrontpage(slideshowContainer){
+    slideshowContainer.find(".slideshow").hide();
+    slideshowContainer.find(".slideshow-frontpage").show();
 }
 
-function setStateToSlideshow(){
+function setStateToSlideshow(slideshowContainer){
     location.hash = "1";
-    $("#slideshow-container .slideshow .slide")
+    slideshowContainer.find(".slideshow .slide")
         .hide()
         .first()
         .show();
-    updateSlideshowState();
-    $("#slideshow-container .slideshow-frontpage").hide();
-    $("#slideshow-container .slideshow").show();
+    updateSlideshowState(slideshowContainer);
+    slideshowContainer.find(".slideshow-frontpage").hide();
+    slideshowContainer.find(".slideshow").show();
+    updateSlideshowProgressBar(slideshowContainer);
 }
 
-function startSlideshow(){
+function startSlideshow(slideshowContainer){
     // Wind slideshow back to first slide
-    $("#slideshow-container .slideshow .slide").sort(function(a,b){
+    slideshowContainer.find(".slideshow .slide").sort(function(a,b){
         var aIndex = parseInt(a.getAttribute("index"));
         var bIndex = parseInt(b.getAttribute("index"));
-        console.log("a: " + aIndex + ", b: " + bIndex + ", sum: " + (aIndex + bIndex));
         return aIndex - bIndex;
-    }).appendTo("#slideshow > .slideshow-slides-group");
-    $("#slideshow-container .slideshow .slideshow-progressbar svg .progressbar-slide").sort(function(a,b){
+    }).appendTo(slideshowContainer.find(".slideshow .slideshow-slides-group"));
+    slideshowContainer.find(".slideshow .slideshow-progressbar svg .progressbar-slide").sort(function(a,b){
         var aIndex = parseInt(a.getAttribute("index"));
         var bIndex = parseInt(b.getAttribute("index"));
-        console.log("a: " + aIndex + ", b: " + bIndex + ", sum: " + (aIndex + bIndex));
         return aIndex - bIndex;
-    }).appendTo("#slideshow-container .slideshow .slideshow-progressbar svg .progressbar-slides-group");
-    $("#slideshow-container .slideshow .slideshow-progressbar svg .progressbar-slide")
+    }).appendTo(slideshowContainer.find(".slideshow .slideshow-progressbar svg .progressbar-slides-group"));
+    slideshowContainer.find(".slideshow .slideshow-progressbar svg .progressbar-slide")
         .attr("fill", "url(#inactiveSlideGradient)")
         .first()
         .attr("fill", "url(#activeSlideGradient)");
-    setStateToSlideshow();
+    setStateToSlideshow(slideshowContainer);
 }
 
-$(window).on("hashchange", function(){
-    if(location.hash == "#1"){
-        setStateToSlideshow();
-    }
-    else {
-        setStateToFrontpage();
-    }
-});
+function prepareSlideshow(slideshowContainer){
+    $(window).on("hashchange", function(){
+        if(location.hash == "#1"){
+            setStateToSlideshow(slideshowContainer);
+        }
+        else {
+            setStateToFrontpage(slideshowContainer);
+        }
+    });
+    $(window).resize(function(){updateSlideshowProgressBar(slideshowContainer);});
+    indexSlides(slideshowContainer);
+    drawFrontpageProgressBar(slideshowContainer);
+    drawSlideshowProgressBar(slideshowContainer);
+    setStateToFrontpage(slideshowContainer);
+}
+
