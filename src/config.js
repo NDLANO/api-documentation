da2ee3ec3612ce72d17e42502ee14ca652fb19ac
Henrik Nårstad <henrik.narstad@knowit.no>
2018-11-27 16:08:43 +0100

    different auth0 personal client for envs #1439

--- src/config.js
+++ src/config.js
@@ -15,12 +15,23 @@ const environment = {
   },
 }[process.env.NODE_ENV || 'development'];
 
+const getAuth0PersonalClient = () => {
+  switch (process.env.NDLA_ENVIRONMENT) {
+    case 'prod':
+      return 'WU0Kr4CDkrM0uL9xYeFV4cl9Ga1vB3JY';
+    case 'staging':
+      return 'fvJHyVEVaVTJ9UpoCBZf2O3xKbA7fDeT';
+    default:
+      return 'FK35FD3YHOeaYcXG80EVCbBmAfiFGziV';
+  }
+};
+
 module.exports = Object.assign({
   host: process.env.API_DOCUMENTATION_HOST || 'localhost',
   port: process.env.API_DOCUMENTATION_PORT || '3000',
   ndlaApiGatewayUrl: process.env.NDLA_API_URL || 'http://api-gateway.ndla-local:8001',
   apiDocPath: new RegExp('api-docs'),
-
+  auth0PersonalClient: getAuth0PersonalClient(),
   app: {
     title: 'NDLA API Documentation',
   },
